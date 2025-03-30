8e9c64de68c7bb54d37d74a9913c58d9d93d132f
AsMathias <AslakMathias@outlook.com>
2025-03-30 01:12:36 +0100

    Initial commit

---
+++ Windows/doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("c:/Users/amx/org/roam/20241208234619-org_mode_notes.org" "c:/Users/amx/org/agenda.org"))
+ '(package-selected-packages '(org-download)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:weight bold))))
+ '(font-lock-keyword-face ((t (:weight bold)))))
